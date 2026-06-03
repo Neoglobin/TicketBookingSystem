11b41054ab6ad060b1ffc4ccc41f05596ed7df42
Neo <levinalex1111@gmail.com>
2026-06-03 16:32:10 +0500

    initial

---
+++ APP/API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+// Add services to the container.
+
+services.AddControllers();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
